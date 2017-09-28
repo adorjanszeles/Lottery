115c900bdf022499f1b6aa80f3f581ad3f433f38
iipepeii <GitHub555ttt>
2017-09-28 12:29:44 +0200

    SZERENCSE-9, SZERENCSE-10

---
+++ src/main/java/com/lottery/model/WeeklyDrawList.java
@@ -0,0 +1,21 @@
+package com.lottery.model;
+
+import java.util.List;
+
+/**
+ * Wrapper osztály a Drools egy speciális gyengeségének kezelésére,
+ * mivel nem kezeli rendesen az insert-elt collection-öket. Ellenben ha egy wrapper
+ * osztály field-jén kell iterálnia, úgy tökéltesen működik.
+ */
+public class WeeklyDrawList {
+
+    private List<WeeklyDraw> weeklyDrawList;
+
+    public List<WeeklyDraw> getLotteryPreparedForDrools() {
+        return this.weeklyDrawList;
+    }
+
+    public void setLotteryPreparedForDrools(Lottery lottery) {
+        this.weeklyDrawList = lottery.getLotteryList();
+    }
+}
