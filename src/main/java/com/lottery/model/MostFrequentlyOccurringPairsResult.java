f67bd70590e0890a87c28f33ea23a9fd50faee50
iipepeii <szrk.peter@gmail.com>
2017-09-28 12:29:44 +0200

    SZERENCSE-9, SZERENCSE-10

---
+++ src/main/java/com/lottery/model/MostFrequentlyOccurringPairsResult.java
@@ -0,0 +1,17 @@
+package com.lottery.model;
+
+/**
+ * A Most Frequently Occurring Pairs rule eredményeit gyüjtő osztály.
+ */
+public class MostFrequentlyOccurringPairsResult {
+
+    private Integer[] result;
+
+    public Integer[] getResult() {
+        return result;
+    }
+
+    public void setResult(Integer[] result) {
+        this.result = result;
+    }
+}
