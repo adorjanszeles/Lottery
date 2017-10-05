b852d72eda43e221849c36222604902524b8d23f
martikecskemeti <martikecskemeti@gmail.com>
2017-10-05 15:06:38 +0200

    SZERENCSE-20-24-27

--- src/main/java/com/lottery/kie/KieServiceImpl.java
+++ src/main/java/com/lottery/kie/KieServiceImpl.java
@@ -40,6 +40,7 @@ public class KieServiceImpl implements KieService {
             throw new MissingKieServicesException("Hiányzó com.lottery.kie session");
         }
         KieServiceImpl.LOGGER.debug("kieSession sikeresen létrejött");
+        this.kSession.setGlobal("LOGGER", LOGGER);
         return this.kSession;
 
     }
