1a4d624412b7a6ca9887bc8f42585bdac21aa12a
gasparrobi <gasparrobi@gmail.com>
2017-10-18 12:56:58 +0200

    Merge branch 'feature/gasparrobi_unit_drools_refactor' into development
    # Conflicts:
    #	src/main/java/com/lottery/service/MostFrequentFiveNumberServiceImpl.java
    #	src/main/resources/rules/most_frequent_five_number.drl
    #	src/test/java/com/lottery/service/TestAverageTimeBetweenTwoMatchFiveDraws.java
    #	src/test/java/com/lottery/service/TestDrawAverage.java
    #	src/test/java/com/lottery/service/TestFourMatchRatioToFiveMatch.java
    #	src/test/java/com/lottery/service/TestMostFrequentlyOccurringPairsRule.java
    #	src/test/java/com/lottery/service/TestRearestFiveNumberRule.java

--- lottery-model/src/main/java/com/lottery/model/AverageResult.java
+++ lottery-model/src/main/java/com/lottery/model/AverageResult.java
@@ -1,15 +1,10 @@
 package com.lottery.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 /**
  * Result segédosztály a Drools működésének könnyítéséhez, amely a kihúzott számok átlagát tartalmazza
  */
-@ApiModel("Average Number Result")
 public class AverageResult {
 
-    @ApiModelProperty(value = "average number", required = true)
     private Float result;
 
     public AverageResult() {
