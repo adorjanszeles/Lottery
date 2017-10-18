7e6c1ecb2e1f95185b475b265fc1be45803f6e6c
gasparrobi <gasparrobi@gmail.com>
2017-10-18 12:56:58 +0200

    Merge branch 'feature/gasparrobi_unit_drools_refactor' into development
    # Conflicts:
    #	src/main/java/com/lottery/service/MostFrequentFiveNumberServiceImpl.java
    #	src/main/resources/rules/most_frequent_five_number.drl
    #	src/test/java/com/lottery/service/TestAverageTimeBetweenTwoMatchFiveDraws.java
    #	src/test/java/com/lottery/service/TestDrawAverage.java
    #	src/test/java/com/lottery/service/TestFourMatchRatioToFiveMatch.java
    #	src/test/java/com/lottery/service/TestMostFrequentlyOccurringPairsRule.java
    #	src/test/java/com/lottery/service/TestRearestFiveNumberRule.java

--- lottery-model/src/main/java/com/lottery/model/RearestFiveResult.java
+++ lottery-model/src/main/java/com/lottery/model/RearestFiveResult.java
@@ -1,18 +1,13 @@
 package com.lottery.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Result segédosztály a Drools működésének könnyítéséhez, amely a legritkább 5 számból álló listát tartalmazza
  */
-@ApiModel("Rearest Five Number Result")
 public class RearestFiveResult {
 
-    @ApiModelProperty(value = "list of rarest numbers", required = true)
     private List<Integer> result;
 
     public RearestFiveResult() {
