0236c5a8d426e31f22f42c1626dd6e2bf8029dd2
martikecskemeti <martikecskemeti@gmail.com>
2017-10-18 09:35:28 +0200

    SZERENCSE-57

--- src/main/java/com/lottery/service/AverageServiceImpl.java
+++ src/main/java/com/lottery/service/AverageServiceImpl.java
@@ -3,10 +3,9 @@ package com.lottery.service;
 import com.lottery.config.LotteryQualifier;
 import com.lottery.model.AverageResult;
 import com.lottery.model.Lottery;
+import com.lottery.model.WeeklyDraw;
 import com.lottery.model.WeeklyDrawList;
 import org.kie.api.runtime.StatelessKieSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,35 +13,50 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * {@link AverageService interfész implementációja.
+ * {@link AverageService interfész implementációja. {@link LotteryService osztály extendálása.
  */
 
 @Service
-public class AverageServiceImpl implements AverageService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AverageServiceImpl.class);
-    private Lottery lottery;
+public class AverageServiceImpl extends LotteryService implements AverageService {
     private StatelessKieSession kieSession;
 
     @Autowired
     public AverageServiceImpl(@Qualifier(LotteryQualifier.statelessKieSessionName) StatelessKieSession kieSession,
                               Lottery lottery) {
+        super(lottery);
         this.kieSession = kieSession;
-        this.lottery = lottery;
     }
 
     @Override
     public AverageResult executeRule() {
-        AverageServiceImpl.LOGGER.debug("szabály futtatása elkezdődött...");
-        WeeklyDrawList weeklyDrawList;
-        AverageResult averageResult;
-        weeklyDrawList = new WeeklyDrawList();
-        weeklyDrawList.setDrawListPreparedForDrools(this.lottery.getLotteryList());
-        averageResult = new AverageResult();
+        AverageServiceImpl.LOGGER.debug("Átlagot számító szabály futtatása elkezdődött...");
+        WeeklyDrawList weeklyDrawList = super.init();
+        AverageResult averageResult = this.execute(weeklyDrawList);
+        AverageServiceImpl.LOGGER.debug("Átlagot számító szabály futtatása befejeződött...");
+        return averageResult;
+    }
+
+    @Override
+    public AverageResult executeRuleFilterByDate(String from, String to) {
+        AverageServiceImpl.LOGGER.debug("Datum alapjan filterezett átlagot számító service futtatása elkezdődött...");
+        WeeklyDrawList weeklyDrawList = super.init();
+        List<WeeklyDraw> filteredList = weeklyDrawList.getDrawListPreparedForDrools()
+                                                      .stream()
+                                                      .filter(draw -> super.filterByDate(draw, from, to) != null)
+                                                      .collect(Collectors.toList());
+        weeklyDrawList.setDrawListPreparedForDrools(filteredList);
+        AverageResult averageResult = this.execute(weeklyDrawList);
+        AverageServiceImpl.LOGGER.debug("Datum alapjan filterezett átlagot számító service futtatása befejeződött...");
+        return averageResult;
+    }
+
+    private AverageResult execute(WeeklyDrawList weeklyDrawList) {
+        AverageResult averageResult = new AverageResult();
         List<Object> facts = new ArrayList<>(Arrays.asList(weeklyDrawList, averageResult));
         this.kieSession.execute(facts);
-        AverageServiceImpl.LOGGER.debug("szabály futtatása befejeződött...");
         return averageResult;
     }
 }
--- src/main/java/com/lottery/service/FourMatchRatioToFiveServiceImpl.java
+++ src/main/java/com/lottery/service/FourMatchRatioToFiveServiceImpl.java
@@ -3,10 +3,9 @@ package com.lottery.service;
 import com.lottery.config.LotteryQualifier;
 import com.lottery.model.FourMatchRatioToFiveMatchResult;
 import com.lottery.model.Lottery;
+import com.lottery.model.WeeklyDraw;
 import com.lottery.model.WeeklyDrawList;
 import org.kie.api.runtime.StatelessKieSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,33 +13,51 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * {@link FourMatchRatioToFiveService interfész implementációja.
+ * {@link FourMatchRatioToFiveService interfész implementációja. {@link LotteryService osztály extendálása.
  */
 @Service
-public class FourMatchRatioToFiveServiceImpl implements FourMatchRatioToFiveService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FourMatchRatioToFiveServiceImpl.class);
-    private Lottery lottery;
+public class FourMatchRatioToFiveServiceImpl extends LotteryService implements FourMatchRatioToFiveService {
     private StatelessKieSession kieSession;
 
     @Autowired
     public FourMatchRatioToFiveServiceImpl(
             @Qualifier(LotteryQualifier.statelessKieSessionName) StatelessKieSession kieSession, Lottery lottery) {
+        super(lottery);
         this.kieSession = kieSession;
-        this.lottery = lottery;
     }
 
     @Override
     public FourMatchRatioToFiveMatchResult executeRule() {
-        FourMatchRatioToFiveServiceImpl.LOGGER.debug("szabály futtatása elkezdődött...");
-        WeeklyDrawList weeklyDrawList = new WeeklyDrawList();
-        weeklyDrawList.setDrawListPreparedForDrools(this.lottery.getLotteryList());
-        System.out.println(weeklyDrawList.getDrawListPreparedForDrools().size());
+        FourMatchRatioToFiveServiceImpl.LOGGER.debug("Négyesek aránya az ötösökhöz szabály futtatása elkezdődött...");
+        WeeklyDrawList weeklyDrawList = super.init();
+        FourMatchRatioToFiveMatchResult fourMatchRatioToFiveMatchResult = this.execute(weeklyDrawList);
+        FourMatchRatioToFiveServiceImpl.LOGGER.debug("Négyesek aránya az ötösökhöz szabály futtatása befejeződött...");
+        return fourMatchRatioToFiveMatchResult;
+    }
+
+    @Override
+    public FourMatchRatioToFiveMatchResult executeRuleFilterByDate(String from, String to) {
+        FourMatchRatioToFiveServiceImpl.LOGGER.debug(
+                "Datum alapjan filterezett négyesek aránya az ötösökhöz service futtatása elkezdődött...");
+        WeeklyDrawList weeklyDrawList = super.init();
+        List<WeeklyDraw> filteredList = weeklyDrawList.getDrawListPreparedForDrools()
+                                                      .stream()
+                                                      .filter(draw -> super.filterByDate(draw, from, to) != null)
+                                                      .collect(Collectors.toList());
+        weeklyDrawList.setDrawListPreparedForDrools(filteredList);
+        FourMatchRatioToFiveMatchResult fourMatchRatioToFiveMatchResult = this.execute(weeklyDrawList);
+        AverageServiceImpl.LOGGER.debug(
+                "Datum alapjan filterezett négyesek aránya az ötösökhöz service futtatása befejeződött...");
+        return fourMatchRatioToFiveMatchResult;
+    }
+
+    private FourMatchRatioToFiveMatchResult execute(WeeklyDrawList weeklyDrawList) {
         FourMatchRatioToFiveMatchResult fourMatchRatioToFiveMatchResult = new FourMatchRatioToFiveMatchResult();
         List<Object> facts = new ArrayList<>(Arrays.asList(weeklyDrawList, fourMatchRatioToFiveMatchResult));
         this.kieSession.execute(facts);
-        FourMatchRatioToFiveServiceImpl.LOGGER.debug("szabály futtatása befejeződött...");
         return fourMatchRatioToFiveMatchResult;
     }
 }
