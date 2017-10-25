3ddd35587a790f44d843b2533cac7572731ee616
martikecskemeti <martikecskemeti@gmail.com>
2017-10-25 14:22:15 +0200

    SZERENCSE-79

---
+++ lottery-web/src/main/java/com/lottery/common/exceptions/ApiGlobalError.java
@@ -0,0 +1,18 @@
+package com.lottery.common.exceptions;
+
+public class ApiGlobalError {
+
+    private String code;
+
+    public ApiGlobalError(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
