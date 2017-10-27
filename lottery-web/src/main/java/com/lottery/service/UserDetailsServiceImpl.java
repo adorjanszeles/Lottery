cd22709cf47e1b804503cd6221897212d7684a30
gasparrobi <gasparrobi@gmail.com>
2017-10-27 10:20:19 +0200

    SZERENCSE-78

---
+++ lottery-web/src/main/java/com/lottery/service/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package com.lottery.service;
+
+import com.lottery.model.User;
+import com.lottery.repository.UserJPARepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+/**
+ * User service osztály a user adatbázisból való betöltéséhez
+ */
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+    private UserJPARepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserJPARepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetailsServiceImpl.LOGGER.debug("SimpleUserServiceImpl user betoltése elkezdődött");
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("username not found:" + username);
+        }
+
+        UserDetailsServiceImpl.LOGGER.debug(
+                "SimpleUserServiceImpl user betöltésének vége. username: " + user.getUsername());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                                                                      AuthorityUtils.createAuthorityList(
+                                                                              "ROLE_" + user.getRole()));
+
+    }
+
+}
