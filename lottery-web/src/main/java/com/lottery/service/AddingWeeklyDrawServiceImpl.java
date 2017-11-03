bed569703b5eeefbd119024a41e14022990b2578
iipepeii <szrk.peter@gmail.com>
2017-11-03 12:55:26 +0100

    SZERENCSE-96

--- lottery-web/src/main/java/com/lottery/service/AddingWeeklyDrawServiceImpl.java
+++ lottery-web/src/main/java/com/lottery/service/AddingWeeklyDrawServiceImpl.java
@@ -2,6 +2,8 @@ package com.lottery.service;
 
 import com.lottery.model.Lottery;
 import com.lottery.model.WeeklyDraw;
+import com.lottery.model.WeeklyDrawDTO;
+import com.lottery.repository.WeeklyDrawJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +15,22 @@ import org.springframework.stereotype.Service;
 public class AddingWeeklyDrawServiceImpl implements AddingWeeklyDrawService {
 
     private Lottery lottery;
+    private WeeklyDrawJPARepository weeklyDrawJPARepository;
+    private WeeklyDrawDestinationMapper weeklyDrawDestinationMapper;
 
     @Autowired
-    public AddingWeeklyDrawServiceImpl(Lottery lottery) {
+    public AddingWeeklyDrawServiceImpl(Lottery lottery, WeeklyDrawJPARepository weeklyDrawJPARepository) {
         this.lottery = lottery;
+        this.weeklyDrawJPARepository = weeklyDrawJPARepository;
     }
 
     @Override
-    public void AddNewWeeklyDraw(WeeklyDraw weeklyDraw) {
+    public void AddNewWeeklyDraw(WeeklyDrawDTO sourceWeeklyDraw) {
+        weeklyDrawDestinationMapper = new WeeklyDrawDestinationMapperImpl();
+        WeeklyDraw weeklyDraw = this.weeklyDrawDestinationMapper.sourceToDestination(sourceWeeklyDraw);
+        System.out.println(weeklyDraw);
         this.lottery.getLotteryList().add(weeklyDraw);
+        weeklyDrawJPARepository.save(weeklyDraw);
     }
 
 }
